fe899eb2f746111b6551c44d027d9ecf3dcc7b42
3zzawi <24IMC11047@fh-krems.ac.at>
2026-04-06 22:46:31 +0200

    Add User class hierarchy for user types

--- src/streaming/users.py
+++ src/streaming/users.py
@@ -10,3 +10,14 @@ Classes to implement:
     - FamilyAccountUser
     - FamilyMember
 """
+class User:
+    def __init__(self, user_id, name):
+        self.user_id = user_id
+        self.name = name
+
+class FreeUser(User): pass
+class PremiumUser(User): pass
+class FamilyMember(User):
+    def __init__(self, user_id, name, age):
+        super().__init__(user_id, name)
+        self.age = age
